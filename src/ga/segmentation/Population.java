f5288d9c43cdc4c91fe3238129cdcd79e059ffe1
Caroline de Pourtales <caro.de.pourtales@gmail.com>
2020-03-08 17:15:17 +0100

    fast non dom sorting finished

--- src/ga/segmentation/Population.java
+++ src/ga/segmentation/Population.java
@@ -1,51 +1,53 @@
 package ga.segmentation;
 
 import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import ga.SimplePopulation;
 
 public class Population extends SimplePopulation {
-	public LinkedList<Population> fastNonDominatedSorting(Population population) {
-		/*LinkedList<Population> fronts = new LinkedList<Population>();
+
+	public LinkedList<LinkedList<Individual>> fastNonDominatedSorting (ga.Population population) {
+		LinkedList<LinkedList<Individual>> fronts = new LinkedList<LinkedList<Individual>>();
 		LinkedList<Individual> F1 = new LinkedList<Individual>();
-		for(IIndividual ind1 : this.getIndividuals()) {
-			Individual i = (Individual) ind1;
+		for (Individual i : this.getIndividuals()) {
 			LinkedList<Individual> Si = new LinkedList<Individual>();
-			int ni = 0;
-			
-			for(IIndividual ind2 : this.getIndividuals()) {
-				Individual j = (Individual) ind2;
-				if(!(i.edgeValue() < j.edgeValue() && i.connectivity()<j.connectivity() && i.overallDeviation() < j.overallDeviation())) {
+			int ni=0;
+			for (Individual j : this.getIndividuals()) {
+				//j dominates i
+				if (!(i.edgeValue() < j.edgeValue() && i.connectivity()<j.connectivity() && i.overallDeviation() < j.overallDeviation())) {
 					ni++;
 				}
 			}
-			if(ni == 0) {
+			if (ni==0) {
 				F1.add(i);
 			}
 		}
 
-		F1.add(fronts);
+		fronts.add(F1);
 		LinkedList<Individual> currentFi = new LinkedList<Individual>(F1);
-		while(!currentFi.isEmpty()) {
+		while (!currentFi.isEmpty()) {
 			LinkedList<Individual> Q = new LinkedList<Individual>();
-			for(Individual i : currentFi) {
+			for (Individual i : currentFi) {
 				LinkedList<Individual> Si = new LinkedList<Individual>();
-				for(Individual j : this.individuals) {
-					if(i.edgeValue() < j.edgeValue() && i.connectivity()<j.connectivity() && i.overallDeviation() < j.overallDeviation()) {
+				for (Individual j : this.getIndividuals()) {
+					if (i.edgeValue() < j.edgeValue() && i.connectivity()<j.connectivity() && i.overallDeviation() < j.overallDeviation()) {
 						Si.add(j);
 					}
 				}
 				int nj=0;
-				for(Individual j : Sp) {
+				for (Individual j : Si) {
 					nj--;
-					if(nj==0) {
+					if (nj==0) {
 						Q.add(j);
 					}
 				}
 			}
 			currentFi = new LinkedList<Individual>(Q);;
 			fronts.add(currentFi);
-		}*/
-		return new LinkedList<Population>();
+		}
+		return fronts;
 	}
+
 }
